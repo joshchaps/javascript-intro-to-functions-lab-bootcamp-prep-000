8fc8c5ac53642f3f60d5c6fa7aa7e52dbfc8ce4e
Joshua Chapman <jwchapmansocial@gmail.com>
2018-08-26 21:33:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   var string = ('hello')
-  return "I can't hear you!"
+  console.log("I can't hear you!")
   
   string = 'HELLO'
   return "YES INDEED!"
